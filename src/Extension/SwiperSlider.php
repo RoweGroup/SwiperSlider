2ef37ab0d326a9c3e52acca3a8fd45a33590c851
Marcus Delion <marcus@tomrowe.com>
2025-11-24 09:32:29 -0600

    adjust css class names

--- src/Extension/SwiperSlider.php
+++ src/Extension/SwiperSlider.php
@@ -79,6 +79,7 @@ class SwiperSlider extends Extension
             'Lazy',
             'Autoplay',
             'AutoplayDelay',
+            'AutoplayProgress',
             'Speed',
             'Slides'
         ]);
